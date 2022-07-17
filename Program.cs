427be614438d8795833420da60ab4f39c4571d0e
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-17 20:45:13 +0700

    Guessing Game

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassesAndObjects
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string secretWord = "Taqi";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess:");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }else
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
